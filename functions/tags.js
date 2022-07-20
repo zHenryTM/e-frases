d2bff965c0bef597c1682b69b80b555bad819ec4
Asheley Henrique <ahenrique301204@gmail.com>
2022-07-19 23:07:30 -0300

    Fix tagFormatted function

--- functions/tags.js
+++ functions/tags.js
@@ -1,7 +1,7 @@
 function tagsFormatted(tag) {
     tag = tag.toLowerCase()
     tag = tag.normalize('NFD').replace(/[\u0300-\u036f]/g, "");
-    tag = tag.replaceAll(",", "")
+    tag = tag.replaceAll(/([.*+?^=!:${}()|\[\]\/\\])/g, "")
     tag = tag.split(" ")     
 
     return tag
