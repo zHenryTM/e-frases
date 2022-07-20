c4c1fe7da748ed23c2d8b37cb548b99542cf414b
Asheley Henrique <ahenrique301204@gmail.com>
2022-07-19 22:21:06 -0300

    Fixing Footer Bottom Position and SlideDown

--- controllers/home/indexController.js
+++ controllers/home/indexController.js
@@ -3,13 +3,12 @@ require("dotenv").config()
 const router = require("express").Router()
 const Post = require("./../../models/Post")
 const jwt = require("jsonwebtoken")
+const tagsFormatted = require("../../functions/tags")
 
 router.get("/", async (req, res) => {
     try {
         const posts = await Post.find().populate("author", "-password").sort({createdAt: -1})
         const auth = req.cookies.token
-
-        console.log(auth)
             
         if(!auth) {
             console.log("Sem auth")
@@ -29,6 +28,36 @@ router.get("/", async (req, res) => {
         console.log("Error showing posts" + err)
         res.send("Error showing posts")
     }
+})
+
+router.post("/filter/search", async (req, res) => {
+    let search = req.body.search
+    search = tagsFormatted(search)
+
+    let posts = await Post.find({tags: search})
+
+    if(posts) {
+        const auth = req.cookies.token
+        
+        if(!auth) {
+            console.log("Sem auth")
+            res.render("index", {posts: posts, isAuth: false})
+        } else {
+            jwt.verify(req.cookies.token, process.env.JWT_SECRET, (err, decoded) => {
+                if(err) {
+                    console.log("Erro ao verificar - Token inválido")
+                    res.render("index", {post: posts, isAuth: false})
+                } else {
+                    console.log("Está autenticado!")
+                    res.render("index", {posts: posts, isAuth: true})
+                }
+            })
+        }
+    } else {
+        res.redirect("/")
+    }
+
+    
 })
 
 module.exports = app => app.use(router)
---
+++ public/js/index/index.js
@@ -0,0 +1,12 @@
+
+    document.addEventListener('scroll', function(e) {
+        let documentHeight = document.body.scrollHeight;
+        let currentScroll = window.scrollY + window.innerHeight;
+        // When the user is [modifier]px from the bottom, fire the event.
+        let modifier = 200; 
+        if(currentScroll + modifier > documentHeight) {
+            $(".footer").slideUp()
+        } else {
+            $(".footer").slideDown()
+        }
+    })
